95a7bdd1d8d7d4d13b083e0030fabaa7eca48541
NikolayKydrushov <163329496+NikolayKydrushov@users.noreply.github.com>
2024-11-23 21:49:13 +0300

    Add files via upload

---
+++ module_4_2..py
@@ -0,0 +1,10 @@
+def inner_function():
+    print("Я в области видимости функции test_function")
+
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+
+test_function()
